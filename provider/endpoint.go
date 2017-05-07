6ca57044404fc211443f8a38a6b0d77b0ab681b3
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2017-05-07 15:22:51 +0900

    Add provider package

---
+++ provider/endpoint.go
@@ -0,0 +1,6 @@
+package provider
+
+type Endpoint struct {
+	AuthURL  string
+	TokenURL string
+}
---
+++ provider/salesforce.go
@@ -0,0 +1,6 @@
+package provider
+
+var Salesforce = &Endpoint{
+	AuthURL:  "https://login.salesforce.com/services/oauth2/authorize",
+	TokenURL: "https://login.salesforce.com/services/oauth2/token",
+}
